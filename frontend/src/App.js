ec965ead6ef62d25feda3cc20783cd0989b4041c
vorobeybird <vorobeybird@gmail.com>
2021-10-17 15:12:30 +0300

    test path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          We succesfully redeployed this image
+          We succesfully redeployed this image !!
         </p>
         <a
           className="App-link"
