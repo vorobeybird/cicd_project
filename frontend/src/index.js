887048d04169a1dd722e433b4a28117c68e5b21c
Varabei Kanstantsin <46844648+vorobeybird@users.noreply.github.com>
2022-02-07 07:40:53 +0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <App />
+  <div> Changes </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
